7d71e50fe0db81006ed81a21e66bb3881ba5fa57
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-13 23:54:14 -0300

    agregue el maquetado completo de la pagina de Inicio y agregue las imagenes para las card de los productos

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import "./App.css";
 import Footer from "./components/common/Footer";
 import Menu from './components/common/Menu'
-import Error404 from './components/pages/Inicio'
+import Error404 from './components/pages/Error404'
 import Administrador from './components/pages/Administrador'
 import Inicio from './components/pages/Inicio'
 import { Container } from 'react-bootstrap';
@@ -13,8 +13,8 @@ function App() {
   <Container className='contenedorInicial container-fluid'>
 
   </Container>
-  {/* <Inicio></Inicio>
-  <Administrador></Administrador> */}
+  <Inicio></Inicio>
+  {/* <Administrador></Administrador>  */}
   <Error404></Error404>
   <Footer></Footer>
   </>;
--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -1,11 +1,121 @@
-import React from 'react';
+import banner from "../../assets/banner_def.jpg";
+import cafe from "../../assets/cafe.jpg";
+import te from "../../assets/te.jpg";
+import batido from "../../assets/batido_oreo.jpg";
+import croissant from "../../assets/croissant.jpg";
+import tarta from "../../assets/lemonPie.jpg";
+import cafeHelado from "../../assets/cafe_helado.jpg";
+import { Row, Col, Card, Button, Container } from "react-bootstrap";
 
 const Inicio = () => {
-    return (
-        <div>
-            <p>Aqui va el maquetado del incio</p>
-        </div>
-    );
+  return (
+    <section>
+      <div id="contenBanner">
+        <img
+          src={banner}
+          alt="banner"
+          id="imgBanner"
+          style={{ width: "100%" }}
+        />
+      </div>
+      <Container>
+        <h1 className="my-4 mt-5 fs-1">Nuestros Productos</h1>
+        <hr />
+        <Row className="mb-4">
+          <Col xs={12} md={6} lg={4}>
+            <Card style={{ width: "100%" }} className="mb-3">
+              <Card.Img variant="top" src={cafe} />
+              <Card.Body>
+                <Card.Title>Café americano</Card.Title>
+                <Card.Text>
+                  Descripción: <br />
+                  <b>Precio: $1500</b>
+                </Card.Text>
+                <Card.Footer className="d-flex justify-content-end">
+                  <Button className="btnColor">Ver más</Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Card style={{ width: "100%" }} className="mb-3">
+              <Card.Img variant="top" src={te} />
+              <Card.Body>
+                <Card.Title>Té verde</Card.Title>
+                <Card.Text>
+                  Descripción: <br />
+                  <b>Precio: $1200</b>
+                </Card.Text>
+                <Card.Footer className="d-flex justify-content-end">
+                  <Button className="btnColor">Ver más</Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Card style={{ width: "100%" }} className="mb-3">
+              <Card.Img variant="top" src={batido} />
+              <Card.Body>
+                <Card.Title>Batido Oreo</Card.Title>
+                <Card.Text>
+                  Descripción: <br />
+                  <b>Precio: $2300</b>
+                </Card.Text>
+                <Card.Footer className="d-flex justify-content-end">
+                  <Button className="btnColor">Ver más</Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Card style={{ width: "100%" }} className="mb-3">
+              <Card.Img variant="top" src={croissant} />
+              <Card.Body>
+                <Card.Title>Croissant Nutella</Card.Title>
+                <Card.Text>
+                  Descripción: <br />
+                  <b>Precio: $1000</b>
+                </Card.Text>
+                <Card.Footer className="d-flex justify-content-end">
+                  <Button className="btnColor">Ver más</Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Card style={{ width: "100%" }} className="mb-3">
+              <Card.Img variant="top" src={tarta} />
+              <Card.Body>
+                <Card.Title>Lemon Pie</Card.Title>
+                <Card.Text>
+                  Descripción: <br />
+                  <b>Precio: $1500</b>
+                </Card.Text>
+                <Card.Footer className="d-flex justify-content-end">
+                  <Button className="btnColor">Ver más</Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col xs={12} md={6} lg={4}>
+            <Card style={{ width: "100%" }} className="mb-3">
+              <Card.Img variant="top" src={cafeHelado} />
+              <Card.Body>
+                <Card.Title>Café Helado</Card.Title>
+                <Card.Text>
+                  Descripción: <br />
+                  <b>Precio: $3500</b>
+                </Card.Text>
+                <Card.Footer className="d-flex justify-content-end">
+                  <Button className="btnColor">Ver más</Button>
+                </Card.Footer>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default Inicio;
