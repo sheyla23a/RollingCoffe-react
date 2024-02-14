e35561c83d9b9909b159450854e562fe4ec03b3c
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-13 21:06:01 -0300

    se agrego el nav y los links del nav

--- src/components/common/Menu.jsx
+++ src/components/common/Menu.jsx
@@ -1,11 +1,27 @@
-
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import logo from '../../assets/coffee_Logo.png'
 
 const Menu = () => {
-    return (
-        <div>
-            <p>Aqui agregar el maquetado del menu</p>
-        </div>
-    );
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">
+            <img src={logo} alt="Logo de RollingCoffe" className='img-fluid' width={170} />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Administrador</Nav.Link>
+            <Nav.Link href="#link">Registro</Nav.Link>
+            <Nav.Link href="#link">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default Menu;
