1fce0d12cd8456613f7becfd53369cc802d765a8
sheyla astorga <sheylaastorga1998@gmail.com>
2024-02-26 09:38:53 -0300

    agregue las validaciones del formLogin

--- src/components/pages/MaquetadoLogin.jsx
+++ src/components/pages/MaquetadoLogin.jsx
@@ -1,34 +1,84 @@
-import { Container } from "react-bootstrap";
-import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
-import Form from "react-bootstrap/Form";
+import { Form, Button, Container, Card } from "react-bootstrap";
+import { useForm } from "react-hook-form";
 
-function MaquetadoLogin() {
+const MaquetadoLogin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (usuario) => {};
+
   return (
-    <Container className="my-5">
-      <Card>
-        <Card.Header>
-          <h4>{"Login"}</h4>{" "}
-        </Card.Header>
-
+    <Container className="mainSection">
+      <Card className="my-5">
+        <Card.Header as="h5">Login</Card.Header>
         <Card.Body>
-            <Form.Label>Email</Form.Label>
-          <Card.Text>
-            <Form.Control type="email" placeholder="name@example.com" />
-          </Card.Text>
-            <Form.Label htmlFor="inputPassword5">Password</Form.Label>
-          <Card.Text>
-            <Form.Control
-              type="password"
-              id="inputPassword5"
-              aria-describedby="passwordHelpBlock"
-            />
-          </Card.Text>
-          <Button variant="primary">Ingresar</Button>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Ingrese un email"
+                {...register("email", {
+                  required: "El email es obligatorio",
+                  minLength: {
+                    value: 4,
+                    message: "El email debe contener al menos 4 caracteres",
+                  },
+                  maxLength: {
+                    value: 250,
+                    message:
+                      "El email debe contener como máximo 250 caracteres",
+                  },
+                  pattern: {
+                    value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/,
+                    message:
+                      "Ingrese una dirección de correo electrónico válida",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                {...register("password", {
+                  required: "El password es obligatorio",
+                  minLength: {
+                    value: 8,
+                    message: "el minimo es de 8 caracteres",
+                  },
+                  maxLength: {
+                    value: 12,
+                    message: "el maximo es de 15 caracteres",
+                  },
+                  pattern: {
+                    value: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/,
+                    message:
+                      "El password debe contener al menos una letra mayúscula, una letra minúscula y un número",
+                  },
+                })}
+              />
+              <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+
+            <Button variant="primary" type="submit">
+              Ingresar
+            </Button>
+          </Form>
         </Card.Body>
       </Card>
     </Container>
   );
-}
+};
 
 export default MaquetadoLogin;
